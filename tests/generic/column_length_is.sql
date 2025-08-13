75d9c8c3db6cd331a4f21cb8e77b9fa4e342139f
Sagar k <woloped796@7tul.com>
2025-08-13 05:09:29 +0000

    raw_changes

---
+++ tests/generic/column_length_is.sql
@@ -0,0 +1,7 @@
+{% test test_column_length_is (model, column_name, expected_length) %}
+    SELECT *
+    FROM {{ model }}
+    WHERE LENGTH({{ column_name }}) != {{ expected_length }}
+{% endtest %}
+
+test_column_length_is
